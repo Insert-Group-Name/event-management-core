2745ba0d4d0dcb6aa9790f22d8222df944a59fdf
Ranil Wallace <raniljwallace@students.utech.edu.jm>
2025-03-15 22:40:33 -0500

    created event seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Create a test user
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        // Create additional users
+        User::factory(5)->create();
+
+        // Run the event seeder
+        $this->call([
+            EventSeeder::class,
         ]);
     }
 }
